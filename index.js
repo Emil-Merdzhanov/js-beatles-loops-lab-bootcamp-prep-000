29d02d6c46a7341cd3c2eb0077a8d0af42987120
decent-port-4955 <emil@georgetownoliveoil.com>
2019-09-28 02:32:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+
+function theBeatlesPlay(players, instruments) {
+    var allPlayers = [ "John Lennon plays Guitar","Paul McCartney plays Bass Guitar","George Harrison plays Lead Guitar","Ringo Starr plays Drums"];
+    return allPlayers
+    }
+    
+function johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"];
+  return facts
+}
